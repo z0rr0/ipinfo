9407c402a2eef43d5b42239b4da92d3ac07dbd7f
Alexander Zaytsev <me@axv.email>
2020-09-22 23:25:24 +0300

    build container fix

--- container_build.sh
+++ container_build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 CONTAINER="golang:1.15-alpine"
-SOURCES="${GOPATH}/src"
-TARGET="${GOPATH}/bin/alpine"
+SOURCES="${PWD}"
 ATTRS="$(/bin/bash version.sh)"
 IDCMD=$(command -v id)
 DCMD=$(command -v docker)
@@ -13,21 +12,13 @@ if [ -z "$DCMD" ]; then
   exit 1
 fi
 
-rm -rf "${TARGET}"
-mkdir -p "${TARGET}"/bin "${TARGET}"/pkg
-
 $DCMD run --rm --user "${PERM}" \
-  --volume "${SOURCES}":/usr/p/src:ro \
-  --volume "${TARGET}"/pkg:/usr/p/pkg \
-  --volume "${TARGET}"/bin:/usr/p/bin \
-  --workdir /usr/p/src/github.com/z0rr0/ipinfo \
-  --env GOPATH=/usr/p \
+  --volume "${SOURCES}":/usr/app \
+  --workdir /usr/app \
   --env GOCACHE=/tmp/.cache \
-  ${CONTAINER} go install -v -ldflags "${ATTRS}" github.com/z0rr0/ipinfo
+  ${CONTAINER} go build -v -ldflags "${ATTRS}"
 
 if [[ $? -gt 0 ]]; then
   echo "ERROR: build container"
   exit 2
 fi
-
-cp -v "${TARGET}"/bin/ipinfo ./
